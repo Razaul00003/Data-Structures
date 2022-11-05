db705d94bfc6519553b6bfcfadf268a3a85afca7
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-05 22:04:26 +0600

    hash table

---
+++ HashTable.js
@@ -0,0 +1,59 @@
+class HashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+
+  //generate hash value
+  _hash(key) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    }
+    return hash;
+  }
+
+  //set hash
+  set(key, value) {
+    let index = this._hash(key);
+
+    if (!this.dataMap[index]) {
+      this.dataMap[index] = [];
+    }
+    this.dataMap[index].push([key, value]);
+    return this;
+  }
+
+  //get hash
+  get(key) {
+    let index = this._hash(key);
+
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  //key
+  keys() {
+    let allKeys = [];
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0]);
+        }
+      }
+    }
+    return allKeys;
+  }
+}
+
+let myHashTable = new HashTable();
+myHashTable.set("mobile", 3232);
+myHashTable.set("laptop", 3343);
+myHashTable.set("pc", 99);
+console.log(myHashTable.keys());
